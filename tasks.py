1aecde85c6a53c302f349bc4e08be3e79a95f5d7
Henry Wilde <henrydavidwilde@gmail.com>
2020-12-02 18:20:53 +0000

    Finish hierarch. subsec. and write body of density

--- tasks.py
+++ tasks.py
@@ -33,7 +33,7 @@ def spellcheck(c):
     exit_codes = [0]
     for path in article:
 
-        print(f"Checking {path} 📖")
+        print(f"📖 Checking {path}")
         latex = path.read_text()
         aspell_output = subprocess.check_output(
             ["aspell", "-t", "--list", "--lang=en_GB"], input=latex, text=True
